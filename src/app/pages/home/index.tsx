75ed3b18aef523d7e6c265b3a5860a074b311c11
Pedro Braghin <braghindev@gmail.com>
2024-04-03 18:10:12 -0300

    feat: change project framework

---
+++ src/app/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <h1>Page under construction. Please come back later</h1>
+    </div>
+  )
+}
